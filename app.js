db7ffbd6bbebc2e3a6690444e5657f007c80a3c2
kanjurer <kanavb9999@gmail.com>
2022-01-10 21:18:46 -0400

    heroku issue, server added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express, { static } from 'express';
+var express = require('express');
 var app = express();
-app.use(static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
